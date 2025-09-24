b149e39e9610d3df90a091d79712fd9bd838aa5d
ABIDRI Sarra <sarra.abidri@supagro.fr>
2025-09-24 14:41:09 +0200

    Update setup.py: Rename package and update URL

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='package',
+    name='SilexExplorerPy',
     version='0.1.0',
     description='A Python package designed to help researchers extract, visualize, and analyze complex phenotypic and environmental data for in-depth scientific insights.',
     author='Sarra ABIDRI',
-    author_email='sarra.abidri@inrae.fr',
-    url='https://forgemia.inra.fr/OpenSILEX/opensilex-graphql/python-package.git',
+    author_email='opensilex@inrae.fr ',
+    url='https://forge.inrae.fr/OpenSILEX/opensilex-graphql/silexexplorerpy.git',
     packages=find_packages(),
     install_requires=[
         'requests',
